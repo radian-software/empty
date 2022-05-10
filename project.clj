6f8a5381d5b7545bc2ea63ccb1240cbc869d5111
Radon Rosborough <radon.neon@gmail.com>
2022-05-09 17:52:40 -0700

    Update to reflect IP transfer

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject empty/lein-template "1.0.0"
   :description "Absolute bare minimum Leiningen template"
-  :url "https://github.com/raxod502/empty"
+  :url "https://github.com/radian-software/empty"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :eval-in-leiningen true)
