08d404d007c193c94c84770449955a1de174af8b
Radon Rosborough <radon@intuitiveexplanations.com>
2024-08-31 18:05:45 -0700

    Set GPG key explicitly

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :eval-in-leiningen true
-  :release-tasks [["deploy" "clojars"]])
+  :release-tasks [["deploy" "clojars"]]
+  :signing {:gpg-key "E6CED8CD047C9366D490C2784A88E27648CABA42"})
